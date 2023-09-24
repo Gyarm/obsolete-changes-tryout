e8eaa7607724330e43cadcb94c553d8f5c144b4d
Gyarm <23422183+Gyarm@users.noreply.github.com>
2023-09-24 15:38:20 +0200

    Added Addition

--- Calculator.cs
+++ Calculator.cs
@@ -4,6 +4,11 @@ namespace MyCalculatorApp
 {
     class Calculator
     {
+        public int Add(int addent1, int addent2)
+        {
+            return addent1 + addent1;
+        }
+        
         public int Subtract(int minuend, int subtrahend)
         {
             return minuend;
