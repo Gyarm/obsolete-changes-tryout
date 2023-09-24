2cb6e43656dbcbbb2e76edba7c62e267337ab5a2
Gyarm <23422183+Gyarm@users.noreply.github.com>
2023-09-24 15:37:03 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyCalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculator()
+        }
+    }
+}
